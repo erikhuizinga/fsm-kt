09155b257bb11532721cf4a550accd376b582ff5
Erik Huizinga <erik.huizinga@nedap.com>
2020-03-31 10:49:22 +0200

    ♻️ Use data classes

--- fsm/src/State.kt
+++ fsm/src/State.kt
@@ -1,18 +1,10 @@
 sealed class State {
     @Suppress("CanSealedSubClassBeObject")
-    class Idle internal constructor(): State() {
+    class Idle internal constructor() : State() {
         override fun toString() = "Idle"
     }
 
-    class Loading internal constructor(val retries: Int = 0) : State() {
-        override fun toString() = "Loading"
-    }
-
-    class Failure internal constructor(val retries: Int) : State() {
-        override fun toString() = "Failure"
-    }
-
-    class Success internal constructor(val retries: Int) : State() {
-        override fun toString() = "Success"
-    }
+    data class Loading internal constructor(val retries: Int = 0) : State()
+    data class Failure internal constructor(val retries: Int) : State()
+    data class Success internal constructor(val retries: Int) : State()
 }
